89099a2bc995d330804c5717fb28771e7c72b0e6
Liam Murray <liam@murray.to>
2023-04-02 01:39:43 -0500

    Co-authored-by: Joshua Silva <joshuasilva414@users.noreply.github.com>

--- src/pages/game/enter.tsx
+++ src/pages/game/enter.tsx
@@ -1,28 +1,31 @@
 import { type NextPage } from "next";
+import { IoProvider } from "socket.io-react-hook";
 
 const Enter: NextPage = () => {
-  return (
-    <main className="w-screnn flex h-screen flex-col items-center justify-center bg-[url('/img/themes/default/bg.jpeg')] bg-cover">
-      <h1 className="text-center font-sans text-8xl font-black text-white">
-        <span className="bg-gradient-to-b from-gray-500 to-default-orange bg-clip-text text-transparent">
-          Mythic
-        </span>
-        AI
-      </h1>
-      <div className="mt-10 flex aspect-video w-full max-w-[750px] flex-col items-center justify-center rounded border-2 border-default-orange bg-gray-500 bg-opacity-50">
-        <input
-          className="max-w-[200px] rounded p-2"
-          placeholder="Game Code"
-        ></input>
-        <button className="mt-2 max-w-[200px] rounded bg-white p-2 font-bold">
-          Join Room
-        </button>
-        <button className="mt-2 max-w-[200px] rounded bg-white p-2 font-bold">
-          Create Room
-        </button>
-      </div>
-    </main>
-  );
+	return (
+		<IoProvider>
+			<main className="w-screnn flex h-screen flex-col items-center justify-center bg-[url('/img/themes/default/bg.jpeg')] bg-cover">
+				<h1 className="text-center font-sans text-8xl font-black text-white">
+					<span className="bg-gradient-to-b from-gray-500 to-default-orange bg-clip-text text-transparent">
+						Mythic
+					</span>
+					AI
+				</h1>
+				<div className="mt-10 flex aspect-video w-full max-w-[750px] flex-col items-center justify-center rounded border-2 border-default-orange bg-gray-500 bg-opacity-50">
+					<input
+						className="max-w-[200px] rounded p-2"
+						placeholder="Game Code"
+					></input>
+					<button className="mt-2 max-w-[200px] rounded bg-white p-2 font-bold">
+						Join Room
+					</button>
+					<button className="mt-2 max-w-[200px] rounded bg-white p-2 font-bold">
+						Create Room
+					</button>
+				</div>
+			</main>
+		</IoProvider>
+	);
 };
 
 export default Enter;
